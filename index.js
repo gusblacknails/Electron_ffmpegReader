3739129a1a4a54ddf3575e59d8257101c7c15ec0
Gus Escola <camagrock@gmail.com>
2017-08-28 18:50:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const electron = require("electron")
+const { app, BrowserWindow, ipcMain } = electron
+const ffmpeg = require('fluent-ffmpeg')
+let mainWindow
+
+app.on('ready', () => {
+console.log ("Amo pallá!!")
+mainWindow = new BrowserWindow({})
+mainWindow.loadURL(`file://${__dirname}/index.html`)
+})
+
+ipcMain.on('video:submit', (event, path)=>{
+
+ffmpeg.ffprobe(path, (err, metadata)=>{
+	mainWindow.webContents.send('video:metadata' ,
+	 metadata.format.duration)
+})
+
+})
